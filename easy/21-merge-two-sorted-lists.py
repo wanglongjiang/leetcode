8baddac4c06f3c1b32063b920a9cabb6b716d3e4
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-06-21 14:55:53 +0800

    简化代码

--- easy/21-merge-two-sorted-lists.py
+++ easy/21-merge-two-sorted-lists.py
@@ -1,6 +1,7 @@
 '''
 合并两个有序链表
 '''
+from typing import List
 
 
 # Definition for singly-linked list.
@@ -10,44 +11,60 @@ class ListNode:
         self.next = next
 
 
+'''
+思路：链表
+迭代2个链表，如果l1>l2，则将l2插入新的链表，否则将l1插入新的链表，直至2个链表都为空
+
+时间复杂度：O(n)
+空间复杂度：O(1)
+'''
+
+
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = None
-        tail = None
-        while l1 is not None and l2 is not None:
+        head = ListNode(0)
+        tail = head
+        while l1 and l2:
             if l1.val > l2.val:
-                if head is None:
-                    head = l2
-                    tail = l2
-                else:
-                    tail.next = l2
-                    tail = l2
+                tail.next = l2
                 l2 = l2.next
             else:
-                if head is None:
-                    head = l1
-                    tail = l1
-                else:
-                    tail.next = l1
-                    tail = l1
-                l1 = l1.next
-
-        if l1 is not None:
-            if tail is None:
-                return l1
-            else:
                 tail.next = l1
-                return head
-        if l2 is not None:
-            if tail is None:
-                return l2
-            else:
-                tail.next = l2
-                return head
-        return head
+                l1 = l1.next
+            tail = tail.next
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return head.next
+
+
+def fromList(li: List[int]):
+    head = None
+    tail = head
+    for item in li:
+        if head is None:
+            head = ListNode(item)
+            tail = head
+        else:
+            tail.next = ListNode(item)
+            tail = tail.next
+    return head
+
+
+def toList(listNode: ListNode):
+    if listNode is None:
+        return []
+    else:
+        li = []
+        while listNode is not None:
+            li.append(listNode.val)
+            listNode = listNode.next
+        return li
 
 
 s = Solution()
-print(s.mergeTwoLists([1, 2, 4], [1, 3, 4]))
-print(s.mergeTwoLists([], []))
-print(s.mergeTwoLists([], [0]))
+print(toList(s.mergeTwoLists(fromList([1, 2, 4]), fromList([1, 3, 5]))))
+print(toList(s.mergeTwoLists(fromList([2, 4, 6]), fromList([1, 3, 5]))))
+print(toList(s.mergeTwoLists(fromList([]), fromList([]))))
+print(toList(s.mergeTwoLists(fromList([]), fromList([0]))))
