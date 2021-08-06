42aef934b0fef25691e4c24efe0021d23098c343
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-08-06 13:37:43 +0800

    完成日常

--- hard/847-shortest-path-visiting-all-nodes.py
+++ hard/847-shortest-path-visiting-all-nodes.py
@@ -31,11 +31,36 @@ graph.length = N，且只有节点 i 和 j 连通时，j != i 在列表 grap
 '''
 from typing import List
 '''
-思路：位运算 BFS 动态规划 状态压缩
-TODO
+思路：位运算 BFS 状态压缩
+因为最多有12个节点，可以将遍历状态压缩为1个整数。
+最终状态是(1<<n)-1，也就是低位有n个1。
+从每个节点出发，暴力搜索所有的路径，直至所有节点被遍历完毕。
+
+时间复杂度：O(2^n)，最坏情况下遍历n*2^n个状态
+空间复杂度：O(2^n)，最坏情况下需要n*2^n个状态
 '''
 
 
 class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
-        pass
+        n = len(graph)
+        q, nextq, ans = [(i, 1 << i) for i in range(n)], [], 0  # 将所有节点和初始状态加入队列待遍历
+        finish = (1 << n) - 1  # 最终状态，达到这个状态后，所有的节点被遍历完
+        hashset = set()  # 剪枝用，将已遍历过的状态加入哈希表，防止重复遍历
+        while True:
+            node, state = q.pop()
+            if state == finish:
+                return ans
+            for nextnode in graph[node]:  # 遍历当前节点所有下一节点
+                nextState = (nextnode, state | (1 << nextnode))  # 将下一节点的状态附加到当前状态上
+                if nextState not in hashset:  # 剪枝，如果下一个节点的状态未遍历过，加入队列
+                    hashset.add(nextState)
+                    nextq.append(nextState)
+            if not q:
+                q, nextq = nextq, q
+                ans += 1
+
+
+s = Solution()
+print(s.shortestPathLength([[1, 2, 3], [0], [0], [0]]))
+print(s.shortestPathLength([[1], [0, 2, 4], [1, 3, 4], [2], [1, 2]]))
