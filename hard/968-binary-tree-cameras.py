0c514d95f101304dc5c552c6741c9e05624323ee
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-08-19 10:55:29 +0800

    完成一道难题

--- hard/968-binary-tree-cameras.py
+++ hard/968-binary-tree-cameras.py
@@ -28,6 +28,7 @@
 给定树的节点数的范围是 [1, 1000]。
 每个节点的值都是 0。
 '''
+from typing import List
 
 
 # Definition for a binary tree node.
@@ -40,7 +41,7 @@ class TreeNode:
 
 '''
 思路：DFS 贪心算法
-叶子节点的父节点设置为有监控，然后向上隔一个节点设置一个摄像头
+叶子节点的父节点设置为有监控，然后向上隔一个节点设置一个摄像头，需要很多判断，详见代码
 
 时间复杂度：O(n)
 空间复杂度：O(n)
@@ -51,19 +52,62 @@ class Solution:
     def minCameraCover(self, root: TreeNode) -> int:
         ans = 0
 
-        def dfs(node):
+        def dfs(node, hasParent):
             nonlocal ans
-            if not node.left and not node.right:  # 叶子节点不需要设置
-                return False
-            leftHas, rightHas = False, False
+            if not node.left and not node.right:
+                if hasParent:  # 叶子节点不需要设置
+                    return 0
+                ans += 1  # 根节点没有子节点，需要设置
+                return 1
+            left, right = 1, 1
             if node.left:
-                leftHas = dfs(node.left)
+                left = dfs(node.left, True)
             if node.right:
-                rightHas = dfs(node.right)
-            if not leftHas and not rightHas:
+                right = dfs(node.right, True)
+            # 子节点只要有1个为0，即为未监控；子节点只要有1个设置了监控，当前节点就不用设置监控；
+            child = 0 if left == 0 or right == 0 else (2 if left == 2 or right == 2 else 1)
+            if child == 0:  # 0代表子节点至少有1个未被监控，当前节点需要设置
                 ans += 1
-                return True
-            return False
+                return 2
+            if child == 1:  # 1代表子节点都被监控，但不能影响当前节点
+                if hasParent:  # 有父节点，当前节点可以不设置
+                    return 0
+                else:
+                    ans += 1  # 没有父节点，当前节点必须设置监控了
+                    return 2
+            return 1  # 子节点只要有1个设置了监控，当前节点就不用设置监控
 
-        dfs(root)
+        dfs(root, False)
         return ans
+
+
+null = None
+
+
+# list数据按照bfs遍历得到
+def fromList(li: List[int]):
+    if len(li) == 0:
+        return None
+    root = TreeNode(val=li[0])
+    queue = [root]
+    i = 1
+    while i < len(li):
+        node = queue[0]
+        del queue[0]
+        if li[i] is not None:
+            node.left = TreeNode(val=li[i])
+            queue.append(node.left)
+        i += 1
+        if i < len(li):
+            if li[i] is not None:
+                node.right = TreeNode(val=li[i])
+                queue.append(node.right)
+            i += 1
+    return root
+
+
+s = Solution()
+print(s.minCameraCover(fromList([0, 1, 2, null, 3, 4, null, null, 5])))
+print(s.minCameraCover(fromList([0, 1, null, null, 2, 3, null, null, 4, 5])))
+print(s.minCameraCover(fromList([0, 1, 2, null, null, null, 3])))
+print(s.minCameraCover(fromList([0])))
