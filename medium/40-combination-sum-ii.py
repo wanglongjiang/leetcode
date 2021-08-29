3b570b5021c1b32fedb81b763f0b118b9c5a30db
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-08-29 09:55:50 +0800

    完成日常

--- medium/40-combination-sum-ii.py
+++ medium/40-combination-sum-ii.py
@@ -13,27 +13,28 @@ candidates 中的每个数字在每个组合中只能使用一次。
 '''
 from typing import List
 '''
-解题思路：参照上题思路，搜索回溯
+回溯搜索
+1. 排序
+2. 将index指向的数值从target中减掉，使target=target-arr[index]，然后index+1，继续搜索
+3. 或者跳过index，从index+1之后继续搜索target
+4. 重复上面的2，3回溯搜索过程，将target等于0的所有组合加入list
+
+时间复杂度：O(n!)，因为有剪枝的存在，会远远小于
+空间复杂度：O(n)
 '''
 
 
 class Solution:
-    def combinationSum2(self, candidates: List[int],
-                        target: int) -> List[List[int]]:
-        candidates.sort()
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()  # 经过排序，下面的回溯可以进行剪枝
         if sum(candidates) < target:
             return []
         result = []
         combination = []
-        unionSet = set()
 
         def findCombination(target, index):
             if target == 0:
-                # 利用set排重
-                unionId = ','.join(map(str, combination))
-                if unionId not in unionSet:
-                    result.append(combination.copy())
-                    unionSet.add(unionId)
+                result.append(combination.copy())
                 return
             if index == len(candidates):
                 return
@@ -41,18 +42,23 @@ class Solution:
                 combination.append(candidates[index])
                 findCombination(target - candidates[index], index + 1)
                 combination.pop()
-            findCombination(target, index + 1)
+                while index + 1 < len(candidates) and candidates[index] == candidates[index + 1]:  # 剪枝，排除相同的数值、相同的target
+                    index += 1
+                findCombination(target, index + 1)
+            else:
+                return
 
         findCombination(target, 0)
         return result
 
 
 s = Solution()
-print(s.combinationSum2([2, 5, 2, 1, 2], 5))
-print(s.combinationSum2([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 9))
 print(
     s.combinationSum2([
-        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-        1
-    ], 27))
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+    ], 30))
+print(s.combinationSum2([2, 5, 2, 1, 2], 5))
+print(s.combinationSum2([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 9))
+print(s.combinationSum2([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 27))
 print(s.combinationSum2([10, 1, 2, 7, 6, 1, 5], 8))
