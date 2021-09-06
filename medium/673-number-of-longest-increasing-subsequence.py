b8766fb4ab29bbae95bedbf609d9c0f8075fcb61
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-09-06 13:00:23 +0800

    完成修正

--- medium/673-number-of-longest-increasing-subsequence.py
+++ medium/673-number-of-longest-increasing-subsequence.py
@@ -20,7 +20,9 @@ from typing import List
 思路：动态规划
 与通常的求LIS略有不同。这里是求最长递增子序列的个数，因此需要记录计算所有的最长子序列。
 使用动态规划算法，设dp[n]初始为1，对于dp[i]意思是截止nums[i]，最长子序列的长度。
-dp[i] = max(dp[0..i-1])+1，如果dp[0..i-1]中有多个，需要对个数进行累计
+dp[i] = max(dp[0..i-1])+1
+设lisCount[i]为截止i的最长递增子序列的个数，设j满足dp[i]==dp[j]+1，lisCount[i]=满足条件的所有lisCount[j]之和
+最后找到所有dp[i]==maxLen的lisCount[i]之和
 
 时间复杂度：O(n^2)
 空间复杂度：O(n)
@@ -31,21 +33,27 @@ class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [1] * n
-        maxLen, ans = 1, 1
+        lisCount = [0] * n
+        maxLen = 1
         for i in range(n):
             for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    if dp[i] > maxLen:
-                        maxLen = dp[i]
-                        ans = 1
-                    elif dp[i] == maxLen:
-                        ans += 1
-        if maxLen == 1:  # 如果最大递增子序列长度为1，也就是数组所有元素相同或者为递减序列，子序列个数为数组长度
-            ans = n
+                if nums[i] > nums[j]:  # 发现递增序列
+                    if dp[i] < dp[j] + 1:  # 如果序列长度发生增长
+                        dp[i] = dp[j] + 1  # 序列长度+1
+                        lisCount[i] = lisCount[j]  # 最长序列个数设置为前一序列的个数
+                        maxLen = max(maxLen, dp[i])  # 最长长度更新
+                    elif dp[i] == dp[j] + 1:  # 相同序列长度个数累加
+                        lisCount[i] += lisCount[j]
+            if lisCount[i] == 0:
+                lisCount[i] = 1
+        ans = 0
+        for i, lisLen in enumerate(dp):  # 求所有子最长子序列的数量
+            if lisLen == maxLen:
+                ans += lisCount[i]
         return ans
 
 
 s = Solution()
 print(s.findNumberOfLIS([1, 3, 5, 4, 7]))
 print(s.findNumberOfLIS([2, 2, 2, 2, 2]))
+print(s.findNumberOfLIS([1, 2, 4, 3, 5, 4, 7, 2]))
