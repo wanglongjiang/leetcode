ff50c4682adc66a1f66efff9535d999c0b568c41
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-09-28 08:21:03 +0800

    完成日常

---
+++ offer2/066-z1R5dt.py
@@ -0,0 +1,80 @@
+'''
+剑指 Offer II 066. 单词之和
+实现一个 MapSum 类，支持两个方法，insert 和 sum：
+
+MapSum() 初始化 MapSum 对象
+void insert(String key, int val) 插入 key-val 键值对，字符串表示键 key ，整数表示值 val 。如果键 key 已经存在，那么原来的键值对将被替代成新的键值对。
+int sum(string prefix) 返回所有以该前缀 prefix 开头的键 key 的值的总和。
+ 
+
+示例：
+
+输入：
+inputs = ["MapSum", "insert", "sum", "insert", "sum"]
+inputs = [[], ["apple", 3], ["ap"], ["app", 2], ["ap"]]
+输出：
+[null, null, 3, null, 5]
+
+解释：
+MapSum mapSum = new MapSum();
+mapSum.insert("apple", 3);
+mapSum.sum("ap");           // return 3 (apple = 3)
+mapSum.insert("app", 2);
+mapSum.sum("ap");           // return 5 (apple + app = 3 + 2 = 5)
+ 
+
+提示：
+
+1 <= key.length, prefix.length <= 50
+key 和 prefix 仅由小写英文字母组成
+1 <= val <= 1000
+最多调用 50 次 insert 和 sum
+ 
+
+注意：本题与主站 677 题相同： https://leetcode-cn.com/problems/map-sum-pairs/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/z1R5dt
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+'''
+思路：字典树
+创建字典树，insert操作会将key,value插入字典树
+sum操作会遍历所有以prefix开头的词在字典树中的值，然后求值之和
+
+时间复杂度：insert函数为O(n),n为单个词长度；sum函数为O(nk)，n为prefix长度，k为平均每个prefix的单词个数
+'''
+
+
+class MapSum:
+    def __init__(self):
+        self.trie = {}
+        self.kv = {}
+
+    def insert(self, key: str, val: int) -> None:
+        node = self.trie
+        for c in key:
+            if c not in node:
+                node[c] = {}
+            node = node[c]
+        self.kv[id(node)] = val
+
+    def sum(self, prefix: str) -> int:
+        node = self.trie
+        for c in prefix:
+            if c not in node:
+                node = None
+                break
+            node = node[c]
+        if node is not None:
+            return self.sumAll(node)
+        return 0
+
+    # 递归遍历求node的所有子节点值之和
+    def sumAll(self, node):
+        ans = 0
+        if id(node) in self.kv:
+            ans += self.kv[id(node)]
+        for v in node.values():
+            ans += self.sumAll(v)
+        return ans
---
+++ offer2/070-skFtm2.py
@@ -0,0 +1,72 @@
+'''
+剑指 Offer II 070. 排序数组中只出现一次的数字
+给定一个只包含整数的有序数组 nums ，每个元素都会出现两次，唯有一个数只会出现一次，请找出这个唯一的数字。
+
+ 
+
+示例 1:
+
+输入: nums = [1,1,2,3,3,4,4,8,8]
+输出: 2
+示例 2:
+
+输入: nums =  [3,3,7,7,10,11,11]
+输出: 10
+ 
+
+ 
+
+提示:
+
+1 <= nums.length <= 10^5
+0 <= nums[i] <= 10^5
+ 
+
+进阶: 采用的方案可以在 O(log n) 时间复杂度和 O(1) 空间复杂度中运行吗？
+
+ 
+
+注意：本题与主站 540 题相同：https://leetcode-cn.com/problems/single-element-in-a-sorted-array/
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/skFtm2
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+'''
+from typing import List
+'''
+思路：二分查找
+取数组中部坐标mid=(start+end)/2
+> 如果子数组长度==1，则目标即为唯一的元素
+> 如果nums[mid]==nums[mid+1]，
+>> 如果mid-start为偶数，说明目标在mid+1后面，需要查找mid+2..end
+>> 如果mid-start为奇数，说明目标在mid前面，需要查找start..mid
+> 如果nums[mid]==nums[mid-1]，
+>> 如果mid-start为偶数，说明目标在mid-1前面
+>> 如果mid-start为奇数，说明目标在mid后面
+> 如果nums[mid]与两侧值都不同，则目标就是nums[mid]
+
+重复上面过程
+
+时间复杂度：O(logn)
+空间复杂度：O(1)
+'''
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        start, end = 0, len(nums)
+        while end - start > 1:
+            mid = (start + end) // 2
+            if nums[mid] == nums[mid + 1]:
+                if (mid - start) % 2 == 0:
+                    start = mid + 2
+                else:
+                    end = mid
+            elif nums[mid] == nums[mid - 1]:
+                if (mid - start) % 2 == 0:
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            else:
+                return nums[mid]
+        return nums[start]
