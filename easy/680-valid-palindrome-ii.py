4593ccc7dd9c16158707e3fbb95da6f49699b95d
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-10-13 11:41:48 +0800

    fix bug

--- easy/680-valid-palindrome-ii.py
+++ easy/680-valid-palindrome-ii.py
@@ -34,8 +34,8 @@ s 由小写英文字母组成
 将left,right同时向中间移动。
 > 如果s[left]==s[right]，继续移动
 > 如果s[left]!=s[right]，
->> 如果s[left+1]==s[right]，将left向中间移动一步，继续对比
->> 如果s[left]==s[right-1]，将right向中间移动一步，继续对比
+>> 如果s[left+1]==s[right]，将left向中间移动一步，判断[left,right]是否为回文
+>> 如果s[left]==s[right-1]，将right向中间移动一步，判断[left,right]是否为回文
 >> 如果上述条件不满足，说明无论如何移动，字符都不匹配，返回false
 > 上述单独移动left、right的过程只能进行一次，如果发生第次，返回false
 
@@ -48,16 +48,21 @@ class Solution:
     def validPalindrome(self, s: str) -> bool:
         left, right = 0, len(s) - 1
         deleted = False
+
+        def isPalindrome(left, right):
+            while left <= right and s[left] == s[right]:
+                left += 1
+                right -= 1
+            return left > right
+
         while left <= right:
             if s[left] != s[right]:
                 if deleted:
                     return False
-                if s[left + 1] == s[right]:
-                    left += 1
-                    deleted = True
-                elif s[left] == s[right - 1]:
-                    right -= 1
-                    deleted = True
+                if s[left + 1] == s[right] and isPalindrome(left + 1, right):
+                    return True
+                elif s[left] == s[right - 1] and isPalindrome(left, right - 1):
+                    return True
                 else:
                     return False
             else:
