361e0e551158199f0713050dfe52f7c49bfec083
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2021-10-16 23:02:35 +0800

    完成日常

--- hard/282-expression-add-operators.py
+++ hard/282-expression-add-operators.py
@@ -32,83 +32,36 @@ num 仅含数字
 '''
 from typing import List
 '''
-思路1，暴力回溯
-一个长度为n的数字字符串，可供插入运算符的位置有n-1个，运算符有3种，再加上没有运算符，故所有可能的表达式有4^(n-1)个。
-根据提示，n最大是10，所有可能的表达式约2.6*10^5个，可以尝试暴力计算一下。
+思路1，DFS
 
-时间复杂度：O(4^(n-1)*20)，共生成4^(n-1)个表达式，每个表达式计算时间按长度计算约20
-空间复杂度：O(n)
 '''
 
 
 class Solution:
     def addOperators(self, num: str, target: int) -> List[str]:
         n = len(num)
-        if n == 0:
-            return []
-        if n == 1:
-            if int(num) == target:
-                return [num]
-            else:
-                return []
-
-        # 判断表达式计算结果是否为target
-        def isTarget(ops):
-            nstk, opstk = [], []
-            i, m = 0, len(ops)
-            while i < m:
-                if ops[i] == '+' or ops[i] == '-':
-                    opstk.append(ops[i])
-                elif ops[i] == '*':
-                    i += 1
-                    nstk.append(nstk.pop() * int(ops[i]))
-                else:
-                    nstk.append(int(ops[i]))
-                i += 1
-            while opstk:
-                if opstk.pop() == '-':
-                    nstk.append(-nstk.pop() + nstk.pop())
-                else:
-                    nstk.append(nstk.pop() + nstk.pop())
-            return nstk[0] == target
-
-        # 回溯所有可能的表达式
-        def backtrack(index, ops):
-            for op in ('+', '-', '*'):
-                ops.append(op)
-                if num[index] == '0':  # 第index个字符为0，不能与后面的字符组合成整数
-                    ops.append('0')
-                    if index == n - 1:
-                        if isTarget(ops):
-                            ans.append(''.join(ops))
-                    else:
-                        backtrack(index + 1, ops)
-                    ops.pop()
-                else:
-                    for i in range(index, n):  # 当前操作数的长度从index一直到末尾
-                        if i == n - 1:
-                            ops.append(num[index:])
-                            if isTarget(ops):
-                                ans.append(''.join(ops))
-                            ops.pop()
-                        else:
-                            ops.append(num[index:i + 1])
-                            backtrack(i + 1, ops)
-                            ops.pop()
-                ops.pop()
-
         ans = []
-        if num[0] == '0':  # 第1个数字是0，只能将第1个操作数设置为0
-            backtrack(1, ['0'])
-        else:
-            if int(num) == target:
-                ans.append(num)
-            for i in range(1, n):  # 将数字切分成任意数字，作为第1个操作数
-                backtrack(i, [num[:i]])
+
+        def dfs(index, val, preVal, expr):
+            if index == n and val == target:
+                ans.append(expr)
+            for i in range(index + 1, n + 1):
+                v = int(num[index:i])
+                if i - index > 1 and num[index:index + 1] == '0':
+                    continue
+                if index == 0:
+                    dfs(i, val + v, v, expr + num[index:i])
+                    continue
+                dfs(i, val + v, v, expr + '+' + num[index:i])
+                dfs(i, val - v, -v, expr + '-' + num[index:i])
+                dfs(i, val - preVal + preVal * v, preVal * v, expr + '*' + num[index:i])
+
+        dfs(0, 0, 0, '')
         return ans
 
 
 s = Solution()
+print(s.addOperators("123456789", 45))
 print(s.addOperators(num="00", target=0))
 print(s.addOperators(num="123", target=6))
 print(s.addOperators(num="232", target=8))
