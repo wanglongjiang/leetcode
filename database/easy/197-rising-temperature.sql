97453958f776633fafb42c44dbf4b23243b27d7b
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2022-04-04 22:02:06 +0800

    修正错误

--- database/easy/197-rising-temperature.sql
+++ database/easy/197-rising-temperature.sql
@@ -1,5 +1,5 @@
  # 上升的温度 # 编写一个 SQL 查询，来查找与之前（昨天的）日期相比温度更高的所有日期的 id 。
-SELECT  t1.id AS id
-FROM Weather t1,Weather t2
-WHERE t1.RecordDate=t2.RecordDate+1 
-AND t1.Temperature>t2.Temperature 
+select t1.id as id
+from Weather t1,Weather t2
+where DATEDIFF(t1.recordDate, t2.recordDate) = 1
+and t1.Temperature>t2.Temperature
