245a3432eede2fb1a445707d5175e8665de5c861
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2022-05-05 13:33:23 +0800

    完成二分学习计划

--- medium/1498.number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ medium/1498.number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -43,11 +43,40 @@
 1 <= target <= 10^6
 '''
 from typing import List
+import bisect
 '''
-TODO
+思路：排序 双指针
+子序列的最大值和最小值之和，其他介于最大和最小值之间的元素出现在子序列的哪个地方是没有影响的。
+所以可以先对数组进行排序
+然后遍历每个元素nums[i]，如果能找到一个坐标j，满足i<=j，且nums[i]+nums[j]<=target，那么i与i..j之间的元素可以任意组成子序列，子序列的数量是i..j的全组合数量
+
+时间复杂度：O(nlogn)
+空间复杂度：O(1)
 '''
 
 
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
-        pass
+        nums.sort()
+        n = len(nums)
+        ans, m = 0, 10**9 + 7
+        j = n - 1
+        for i in range(n):
+            if j >= n:  # 首次定位用二分查找
+                j = bisect.bisect_right(nums, target - nums[i], i)
+                if j < i:
+                    break
+            else:  # 双指针查找j
+                for j in range(j, i - 1, -1):
+                    if nums[i] + nums[j] <= target:
+                        break
+                else:
+                    break
+            ans = (ans + 2**(j - i)) % m
+        return ans
+
+
+s = Solution()
+print(s.numSubseq(nums=[3, 5, 6, 7], target=9))
+print(s.numSubseq(nums=[3, 3, 6, 8], target=10))
+print(s.numSubseq(nums=[2, 3, 3, 4, 6, 7], target=12))
