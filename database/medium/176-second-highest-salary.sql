96eff869031e0839af2deee55f0fadcec3dafefb
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2022-08-16 15:28:13 +0800

    修正bug

--- database/medium/176-second-highest-salary.sql
+++ database/medium/176-second-highest-salary.sql
@@ -1,5 +1,8 @@
  # 编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
-SELECT  Salary AS SecondHighestSalary
-FROM Employee
-ORDER BY Salary desc
-LIMIT 1,1
+SELECT
+    IFNULL(
+    (SELECT DISTINCT  Salary
+        FROM Employee
+        ORDER BY Salary desc
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
