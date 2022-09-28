79849fd6c7f4301d4deac9a931ba3fe02f5625c5
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2022-09-28 11:20:20 +0800

    动态规划

---
+++ interview/17.09.get-kth-magic-number-lcci.py
@@ -0,0 +1,32 @@
+'''
+面试题 17.09. 第 k 个数
+有些数的素因子只有 3，5，7，请设计一个算法找出第 k 个数。注意，不是必须有这些素因子，而是必须不包含其他的素因子。例如，前几个数按顺序应该是 1，3，5，7，9，15，21。
+
+示例 1:
+
+输入: k = 5
+
+输出: 9
+'''
+'''
+动态规划
+'''
+
+
+class Solution:
+    def getKthMagicNumber(self, k: int) -> int:
+        dp = [0] * (k + 1)
+        dp[1] = 1
+        p3 = p5 = p7 = 1
+
+        for i in range(2, k + 1):
+            num3, num5, num7 = dp[p3] * 3, dp[p5] * 5, dp[p7] * 7
+            dp[i] = min(num3, num5, num7)
+            if dp[i] == num3:
+                p3 += 1
+            if dp[i] == num5:
+                p5 += 1
+            if dp[i] == num7:
+                p7 += 1
+
+        return dp[k]
