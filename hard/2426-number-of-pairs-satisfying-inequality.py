e85b257b4134313a7ff75ed5e0b792a481fdbf5a
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2022-11-15 08:29:44 +0800

    有序集合

---
+++ hard/2426-number-of-pairs-satisfying-inequality.py
@@ -0,0 +1,77 @@
+'''
+2426. 满足不等式的数对数目
+困难
+11
+相关企业
+给你两个下标从 0 开始的整数数组 nums1 和 nums2 ，两个数组的大小都为 n ，同时给你一个整数 diff ，统计满足以下条件的 数对 (i, j) ：
+
+0 <= i < j <= n - 1 且
+nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff.
+请你返回满足条件的 数对数目 。
+
+ 
+
+示例 1：
+
+输入：nums1 = [3,2,5], nums2 = [2,2,1], diff = 1
+输出：3
+解释：
+总共有 3 个满足条件的数对：
+1. i = 0, j = 1：3 - 2 <= 2 - 2 + 1 。因为 i < j 且 1 <= 1 ，这个数对满足条件。
+2. i = 0, j = 2：3 - 5 <= 2 - 1 + 1 。因为 i < j 且 -2 <= 2 ，这个数对满足条件。
+3. i = 1, j = 2：2 - 5 <= 2 - 1 + 1 。因为 i < j 且 -3 <= 2 ，这个数对满足条件。
+所以，我们返回 3 。
+示例 2：
+
+输入：nums1 = [3,-1], nums2 = [-2,2], diff = -1
+输出：0
+解释：
+没有满足条件的任何数对，所以我们返回 0 。
+ 
+
+提示：
+
+n == nums1.length == nums2.length
+2 <= n <= 105
+-104 <= nums1[i], nums2[i] <= 104
+-104 <= diff <= 104
+'''
+from typing import List
+from sortedcontainers import SortedList
+'''
+[TOC]
+
+# 思路
+有序集合
+
+# 解题方法
+将不等式进行变换
+nums1[i] - nums1[j] <= nums2[i] - nums2[j] + diff
+↓
+nums1[i] - nums2[i] <= nums1[j] - nums2[j] + diff
+根据这个不等式，可以先遍历2个数组，求出2个数组的差，
+然后从右向左遍历差值数组，将差d[i]+diff依次加入有序集合sortedList，在加入前，查询d[i]在sortedList中的索引j，j即为满足条件的数对个数
+
+# 复杂度
+- 时间复杂度: 
+> $O(nlog(n))$
+
+- 空间复杂度: 
+> $O(n)$
+'''
+
+
+class Solution:
+    def numberOfPairs(self, nums1: List[int], nums2: List[int], diff: int) -> int:
+        sortedList = SortedList()
+        ans = 0
+        for num in reversed([nums1[i] - nums2[i] for i in range(len(nums1))]):
+            i = sortedList.bisect_left(num)
+            ans += len(sortedList) - i
+            sortedList.add(num + diff)
+        return ans
+
+
+s = Solution()
+assert s.numberOfPairs(nums1=[3, 2, 5], nums2=[2, 2, 1], diff=1) == 3
+assert s.numberOfPairs(nums1=[3, -1], nums2=[-2, 2], diff=-1) == 0
