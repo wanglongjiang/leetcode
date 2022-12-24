c0db086e607f7b2b9bdb252780bd00ca51005ac7
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2022-12-24 21:07:16 +0800

    fixbug

--- interview/08.11.coin-lcci.py
+++ interview/08.11.coin-lcci.py
@@ -40,11 +40,12 @@
 class Solution:
     def waysToChange(self, n: int) -> int:
         coins = [25, 10, 5, 1]
+        m = 10**9 + 7
         dp = [0] * (n + 1)
         dp[0] = 1
         for i in range(len(coins)):
             for j in range(coins[i], n + 1):
-                dp[j] = dp[j] + dp[j - coins[i]]
+                dp[j] = (dp[j] + dp[j - coins[i]]) % m
         return dp[n]
 
 
