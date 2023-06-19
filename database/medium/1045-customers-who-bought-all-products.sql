c9db09d72929bdf75f585130466badfc7a03f5b0
wanglongjiang <wanglongjiang@baicgroup.com.cn>
2023-06-19 09:15:44 +0800

    fixbug

--- database/medium/1045-customers-who-bought-all-products.sql
+++ database/medium/1045-customers-who-bought-all-products.sql
@@ -1,7 +1,7 @@
 SELECT
 	customer_id 
 FROM
-	( SELECT customer_id, count( * ) AS c_count FROM Customer GROUP BY customer_id ) c,
+	( SELECT customer_id,count( * ) AS c_count from (SELECT customer_id,product_key  FROM Customer GROUP BY customer_id,product_key) t GROUP BY customer_id  ) c,
 	( SELECT count( * ) AS all_count FROM Product ) p 
 WHERE
 	c_count = all_count
